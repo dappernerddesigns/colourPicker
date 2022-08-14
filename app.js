04398656b5cad2e337f66fe285a34a7aeb9468ec
Verity Gregory <verity.gregory@gmail.com>
2022-08-14 12:46:51 +0100

    added hex randomiser

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const boxThree = document.getElementById('box-three');
 const colourOne = document.getElementById('colour-one');
 const colourTwo = document.getElementById('colour-two');
 const colourThree = document.getElementById('colour-three');
+const boxFour = document.getElementById('box-four');
+const boxFive = document.getElementById('box-five');
+const boxSix = document.getElementById('box-six');
+const colourFour = document.getElementById('colour-four');
+const colourFive = document.getElementById('colour-five');
+const colourSix = document.getElementById('colour-six');
 // rgb colour randomiser variables
 
 const rgbRandom = () => {
@@ -12,8 +18,13 @@ const rgbRandom = () => {
 	return num;
 };
 
-const changeDiv = (id) => {
-	console.log('single div', id);
+const hexRandom = () => {
+	const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+	const hex = `#${randomColor}`;
+
+	return hex;
+};
+const changeDivRGB = (id) => {
 	if (id === 'One') {
 		boxOne.style.backgroundColor = `rgb(${rgbRandom()},${rgbRandom()}, ${rgbRandom()})`;
 		colourOne.innerText = boxOne.style.backgroundColor;
@@ -29,8 +40,7 @@ const changeDiv = (id) => {
 	}
 };
 
-const changeAllDivs = () => {
-	console.log('all divs');
+const changeAllDivsRGB = () => {
 	boxOne.style.backgroundColor = `rgb(${rgbRandom()},${rgbRandom()}, ${rgbRandom()})`;
 	boxTwo.style.backgroundColor = `rgb(${rgbRandom()},${rgbRandom()}, ${rgbRandom()})`;
 	boxThree.style.backgroundColor = `rgb(${rgbRandom()},${rgbRandom()}, ${rgbRandom()})`;
@@ -38,4 +48,35 @@ const changeAllDivs = () => {
 	colourOne.innerText = boxOne.style.backgroundColor;
 	colourTwo.innerText = boxTwo.style.backgroundColor;
 	colourThree.innerText = boxThree.style.backgroundColor;
+};
+
+const changeDivHex = (id) => {
+	if (id === 'four') {
+		const hex = hexRandom();
+		boxFour.style.backgroundColor = hex;
+		colourFour.innerText = hex;
+	}
+	if (id === 'five') {
+		const hex = hexRandom();
+		boxFive.style.backgroundColor = hex;
+		colourFive.innerText = hex;
+	}
+	if (id === 'six') {
+		const hex = hexRandom();
+		boxSix.style.backgroundColor = hex;
+		colourSix.innerText = hex;
+	}
+};
+
+const changeAllDivsHex = () => {
+	const hexOne = hexRandom();
+	const hexTwo = hexRandom();
+	const hexThree = hexRandom();
+	boxFour.style.backgroundColor = hexOne;
+	boxFive.style.backgroundColor = hexTwo;
+	boxSix.style.backgroundColor = hexThree;
+
+	colourFour.innerText = hexOne;
+	colourFive.innerText = hexTwo;
+	colourSix.innerText = hexThree;
 };
